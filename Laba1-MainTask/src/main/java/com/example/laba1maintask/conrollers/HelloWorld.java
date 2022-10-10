74e46d5760f5169eb65ec42da4f8262e3e4eaa7b
Dichik <omeluan.dima@gmail.com>
2022-10-10 19:01:33 +0300

    laba2: create basic preview for petition

--- Laba1-MainTask/src/main/java/com/example/laba1maintask/conrollers/HelloWorld.java
+++ Laba1-MainTask/src/main/java/com/example/laba1maintask/conrollers/HelloWorld.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorld {
-
     @RequestMapping("/hello")
     public String helloWorld() {
         return "hello.html";
