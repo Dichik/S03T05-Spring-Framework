baff82203afae704bb9562225d14ebf0093bae93
Dichik <omeluan.dima@gmail.com>
2022-11-26 16:07:56 +0200

    tech: update unresolved file changes

---
+++ spring-boot-dao-project/src/main/java/com/example/springbootdaoproject/SpringBootDaoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdaoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDaoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDaoProjectApplication.class, args);
+    }
+
+}
---
+++ spring-boot-dao-project/src/main/java/com/example/springbootdaoproject/entity/User.java
@@ -0,0 +1,7 @@
+package com.example.springbootdaoproject.entity;
+
+public class User {
+    private String email;
+    private String firstName;
+    private String secondName;
+}
